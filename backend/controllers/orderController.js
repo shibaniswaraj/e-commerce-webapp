0ad9b27f15cf3cc7da9eb649adc0c1d2cd1ee1a5
shivani swaraj <88shivaniswaraj@gmail.com>
2023-09-11 10:23:15 +0530

    4:40

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,10 +54,70 @@ exports.getSingleOrder = catchAsyncErrors(async(req,res,next)=>{
 
 //get logged in user's order
 exports.myOrders = catchAsyncErrors(async(req,res,next)=>{
-    const order = await Order.find({user: req.user._id});
+    const orders = await Order.find({user: req.user._id});
 
      res.status(200).json({
         sucess:true,
-        order
+        orders
+     });
+});
+
+//get all orders --admin
+exports.getAllOrders = catchAsyncErrors(async(req,res,next)=>{
+    const orders = await Order.find();
+    let totalAmount =0;
+    orders.forEach(order=>{
+        totalAmount+= order.totalPrice;
+    })
+
+     res.status(200).json({
+        sucess:true,
+        totalAmount,
+        orders
+     });
+});
+
+//update orders --admin
+exports.updateOrder = catchAsyncErrors(async(req,res,next)=>{
+    const order = await Order.findById(req.params.id);
+    if(order.orderStatus === "Delivered"){
+        return next(new ErrorHander("You have already delivered this order",404))
+    }
+
+    order.orderItems.forEach(async(order)=>{
+        await updateStock(order.product, order.quantity);
+    });
+
+    order.orderStatus = req.body.status;
+    if(req.body.status === "Delivered"){
+        order.deliveredAt= Date.now();
+    }
+
+    await order.save({validateBeforeSave: false});
+
+     res.status(200).json({
+        sucess:true,
+        
+     });
+});
+
+async function updateStock(id,quantity)
+{
+    const product = await Product.findById(id);
+    product.stock-=quantity;
+    product.save({validateBeforeSave: false})
+}
+
+//delete orders --admin
+exports.deleteOrder = catchAsyncErrors(async(req,res,next)=>{
+    const order = await Order.findById(req.params.id);
+    if(!order){
+        return next(new ErrorHander("Order not found",404));
+    }
+    await order.remove();
+
+     res.status(200).json({
+        sucess:true,
+        
      });
 });
